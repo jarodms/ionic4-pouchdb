0eb60e6693a16a4828e2f50340563da5f09260cb
Jarod Smith <jarodms@gmail.com>
2018-08-21 16:09:24 -0500

    list of MyNotes

---
+++ README.md
@@ -0,0 +1,5 @@
+ionic start ionic4-pouchdb tabs
+
+npm install pouchdb --save
+
+npm install @types/pouchdb --save --save-exact
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MyNotesProvider } from '../../providers/my-notes/my-notes';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  allNotes: any;
+
+  constructor(public navCtrl: NavController,
+              public myNotes: MyNotesProvider) {
 
   }
 
+  ionViewDidLoad(){
+ 
+    this.myNotes.getMyNotes().then((data) => {
+      this.allNotes = data;
+    });
+ 
+  }
+
+  onEnter(value: string) {    
+    this.myNotes.addNote(value);
+  }
 }
