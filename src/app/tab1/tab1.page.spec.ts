f409afc5bac9acec885b4cc34dee5887d876bf59
Jarod Smith <jarodms@gmail.com>
2019-12-30 15:55:16 -0600

    fix tab1 test

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { Tab1Page } from './tab1.page';
+import { MyNotesProvider } from '../services/my-notes/my-notes';
 
 describe('Tab1Page', () => {
   let component: Tab1Page;
@@ -10,7 +11,10 @@ describe('Tab1Page', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Tab1Page],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        MyNotesProvider
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab1Page);
